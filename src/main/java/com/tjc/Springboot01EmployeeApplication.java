76ed646eb835f24e2853c27a47d4916154e2c5a7
tangjiacheng <jasontttjc@gmail.com>
2020-06-10 20:37:49 +0800

    initialize project

---
+++ src/main/java/com/tjc/Springboot01EmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.tjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01EmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot01EmployeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tjc/Springboot01EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01EmployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
